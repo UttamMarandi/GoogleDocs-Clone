170bacc5c3568b78e7f5306a92bbd3248464a32d
Uttam Marandi <uttammarandi007@gmail.com>
2021-08-30 10:38:57 +0530

    Header section done

---
+++ components/Header.js
@@ -0,0 +1,48 @@
+import Button from "@material-tailwind/react/Button";
+import Icon from "@material-tailwind/react/Icon";
+
+function Header() {
+  return (
+    <header className="sticky top-0 z-50 flex items-center px-4 py-2 shadow-md bg-white">
+      <Button
+        color="gray"
+        buttonType="outline"
+        rounded={true}
+        iconOnly={true}
+        ripple="dark"
+        className=" md:inline-flex h-20 w-20 border-0"
+      >
+        <Icon name="menu" size="3xl" />
+      </Button>
+      <Icon name="description" size="5xl" color="blue" />
+      <h1 className="ml-2 text-gray-700 text-2xl">Docs</h1>
+
+      <div className="mx-5 md:mx-20 flex flex-grow items-center px-5 py-2 bg-gray-100 text-gray-600 rounded-lg focus-within:text-gray-600 focus-within:shadow-md">
+        <Icon name="search" size="3xl" color="gray" />
+        <input
+          type="text"
+          placeholder="Search"
+          className="flex-grow px-5 text-base bg-transparent outline-none "
+        />
+      </div>
+      <Button
+        color="gray"
+        buttonType="outline"
+        rounded={true}
+        iconOnly={true}
+        ripple="dark"
+        className=" ml-5 md:inline-flex h-20 w-20 border-0"
+      >
+        <Icon name="apps" size="3xl" color="gray" />
+      </Button>
+      <img
+        loading="lazy"
+        className="cursor-pointer h-10 w-10 rounded-full ml-2"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Matt_Damon_TIFF_2015.jpg/800px-Matt_Damon_TIFF_2015.jpg"
+        alt=""
+      />
+    </header>
+  );
+}
+
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,21 @@
-import Head from 'next/head'
+import Head from "next/head";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="">
       <Head>
         <title>Google Docs Clone</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <Header />
+      <section className="bg-[#f8f9fa] pb-10 px-10">
+        <div className="max-w-3xl mx-auto">
+          <div className="flex items-center justify-between py-6">
+            <h2 className="text-gray-700 text-lg">Start a new document</h2>
+          </div>
+        </div>
+      </section>
     </div>
-  )
+  );
 }
