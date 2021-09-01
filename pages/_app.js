55302fa07890dd6efaa1b697ed85752546e8056f
Uttam Marandi <uttammarandi007@gmail.com>
2021-09-01 11:39:55 +0530

    implementing progress bar

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,22 @@ import "@material-tailwind/react/tailwind.css";
 import Head from "next/head";
 import { Provider } from "next-auth/client";
 import "../styles.css";
+import ProgressBar from "@badrap/bar-of-progress";
+import { Router } from "next/dist/client/router";
+
+// implementing progress bar
+const progress = new ProgressBar({
+  size: 4,
+  color: "#2196F3",
+  className: "z-50",
+  delay: 100,
+});
+
+//Router has various event , for each event we we want to do the following
+//When a route is clciked , it triggers routeChange start , on that we want the progressbar to start
+Router.events.on("routeChangeStart", progress.start);
+Router.events.on("routeChangeComplete", progress.finish);
+Router.events.on("routeChangeError", progress.finish);
 
 function MyApp({ Component, pageProps }) {
   return (
