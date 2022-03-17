fb87ad8ce1e6dacab5348bba5bb69a0187e4a57a
Uttam Marandi <uttammarandi007@gmail.com>
2022-03-17 23:13:38 +0530

    bug fix : authentication issue : Error 400: invalid_request

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -7,7 +7,7 @@ export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     Providers.Google({
-      clientId: process.env.GOOGlE_CLIENT_ID,
+      client_id: process.env.GOOGlE_CLIENT_ID,
       clientSecret: process.env.GOOGlE_CLIENT_SECRET,
     }),
     // ...add more providers here
